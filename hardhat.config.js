c5b8506b64deb78bb7724529df231cf65ab22730
Jon <jonathon_rylatt@yahoo.com>
2022-06-10 13:39:26 +0800

    Init

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const RINEKBY_RPC_URL =
+    process.env.RINEKBY_RPC_URL ||
+    "https://eth-rinkeby.alchemyapi.io/v2/QZBXdM8JyK8vjYZ9TXRxlJ7BHY-8cKhT"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINEKBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            //accounts: [PRIVATE_KEY],
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.8",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
